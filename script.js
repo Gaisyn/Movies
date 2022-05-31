51ef39a3ce9807a712ae8a1b1cf3bfbcce558d07
Alexandr <agaisyn@gmail.com>
2022-06-01 02:18:22 +0300

    second commit

--- script.js
+++ script.js
@@ -10,13 +10,64 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?'),
-      b = prompt('На сколько оцените его?'),
-      c = prompt('Один из последних просмотренных фильмов?'),
-      d = prompt('На сколько оцените его?');
+// let i = 0;
+// while (i < 2) {
+//     const a = prompt('Один из последних просмотренных фильмов?'),
+//           b = prompt('На сколько оцените его?');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+//     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//         personalMovieDB.movies[a] = b;
+//         console.log('done');
+//     } else {
+//         console.log('error');
+//         i--;
+//     }
+//     i++;
+// }
+
+// let i = 0;
+// do {
+//     const a = prompt('Один из последних просмотренных фильмов?'),
+//           b = prompt('На сколько оцените его?');
+
+//     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//         personalMovieDB.movies[a] = b;
+//         console.log('done');
+//     } else {
+//         console.log('error');
+//         i--;
+//     }
+//     i++; 
+// }    
+// while (i < 2);
+ 
+
+
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?'),
+          b = prompt('На сколько оцените его?');
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+}
+
+if (personalMovieDB.count < 10) {
+    console.log('Просмотрено мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('Вы класический зритель');
+} else if (personalMovieDB.count >= 30) {
+    console.log('Вы киноман');
+} else {
+    console.log('Произошла ошибка');
+}
+
 console.log(personalMovieDB);
+
+
 
 
